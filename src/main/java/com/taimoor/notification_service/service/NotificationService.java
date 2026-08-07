b0005f1da01b52b89475eadf9e3abe4ee57004f3
Taimoor Aleem <taimooraleem01@gmail.com>
2026-08-07 19:35:51 -0400

    feat(email-server): Setup SMTP using MailTrap to send confirmation email upon placing order

--- src/main/java/com/taimoor/notification_service/service/NotificationService.java
+++ src/main/java/com/taimoor/notification_service/service/NotificationService.java
@@ -1,7 +1,6 @@
 package com.taimoor.notification_service.service;
 
-import com.taimoor.notification_service.order.OrderPlacedEvent;
-import jakarta.mail.internet.MimeMessage;
+import com.taimoor.order_service.event.OrderPlacedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,22 +16,25 @@ import org.springframework.stereotype.Service;
 public class NotificationService {
     private final JavaMailSender javaMailSender;
 
-    @KafkaListener(topics = "order-placed")
+    @KafkaListener(topics = "order-placed-event")
     public void listen(OrderPlacedEvent orderPlacedEvent) {
         log.info("Got message from order-placed topic: {}", orderPlacedEvent);
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom("springshop@email.com");
-            messageHelper.setTo(orderPlacedEvent.getEmail());
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
             messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
             messageHelper.setText(String.format("""
-                    Hi,
+                    Hi %s %s,
                     
                     Your order with order number %s is now placed successfully.
                     
                     Best Regards,
                     Spring Shop
-                    """, orderPlacedEvent.getOrderNumber()));
+                    """,
+                    orderPlacedEvent.getFirstName(),
+                    orderPlacedEvent.getLastName(),
+                    orderPlacedEvent.getOrderNumber()));
         };
         try {
             javaMailSender.send(messagePreparator);
